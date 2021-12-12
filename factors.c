d65c8e770e9a45cc2c9d62b17104e28d8a5a58fc
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-12 01:10:43 -0800

    done!

--- factors.c
+++ factors.c
@@ -15,44 +15,44 @@
  */
 int main(int ac, char **av)
 {
-	int i, j, flag, fd, h;
-	int leido = 0;
-	long int number, number2, k;
-	char *buf, *buf2;
-	int letters = 100;
+int i, j, flag, fd, h;
+int leido = 0;
+long int number, number2, k;
+char *buf, *buf2;
+int letters = 100;
 
-	leido = leido;
-	ac = ac;
-	number = 0;
-	fd = open(av[1], O_RDONLY);
-	buf = malloc(1000 * sizeof(char));
-	buf2 = malloc(120 * sizeof(char));
-	leido = read(fd, buf, letters);
-	i = 0;
-	while (buf[i] != 0)
-	{
-		j = 0;
-		for (h = 0; h <= 100; h++)
-			buf2[h] = 0;
-		while (buf[i] != 10)
-			 {
-				 buf2[j] = buf[i];
-				 i++;
-				 j++;
-			 }
-		number = atol(buf2);
-		flag = 0;
-		for (k = 2; k < number / 2 && flag == 0; k++)
-		{
-			if (number % k == 0)
-				flag = 1;
-		}
-		number2 = number / (k - 1);
-		printf("%li=%li*%li\n", number, number2, k - 1);
-		i++;
-	}
-	free(buf);
-	free(buf2);
-	close(fd);
-	return (0);
+leido = leido;
+ac = ac;
+number = 0;
+fd = open(av[1], O_RDONLY);
+buf = malloc(1000 * sizeof(char));
+buf2 = malloc(120 * sizeof(char));
+leido = read(fd, buf, letters);
+i = 0;
+while (buf[i] != 0)
+{
+j = 0;
+for (h = 0; h <= 100; h++)
+buf2[h] = 0;
+while (buf[i] != 10)
+{
+buf2[j] = buf[i];
+i++;
+j++;
+}
+number = atol(buf2);
+flag = 0;
+for (k = 2; k < number / 2 && flag == 0; k++)
+{
+if (number % k == 0)
+flag = 1;
+}
+number2 = number / (k - 1);
+printf("%li=%li*%li\n", number, number2, k - 1);
+i++;
+}
+free(buf);
+free(buf2);
+close(fd);
+return (0);
 }
